98cb9be73ad5f04d7817148a31555887bec555d1
tanbbang01@gmail.com <tanbbang01@gmail.com>
2021-07-07 22:44:09 +0900

    First commit. Section2 Lecture 19

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Hwang In Seong on 2021/07/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,1 @@
+#I saw Udemy. IOS-IAmRichApp
